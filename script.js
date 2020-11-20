7fc42bdf4c4f8d00e08602d6f53d6ab1bcf5a47b
kamjad1 <73908177+kamjad1@users.noreply.github.com>
2020-11-20 00:20:20 -0600

    working with JSON

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {
+            //console.log(json);
+           let container = document.getElementById("container");
+           json.sort(function(a, b){
+               return a.hoursInSpace - b.hoursInSpace;
+           });
+//let allAstronaut = "";
+for (astronaut in json) {
+    let activeColor = "black"
+    if (json[astronaut].active) {
+        activeColor = "green";
+    }
+   container.innerHTML +=
+
+        `<div class="astronaut">
+   <div class="bio">
+      <h3>${json[astronaut].firstName} ${json[astronaut].lastName}</h3>
+      <ul>
+         <li>Hours in space: ${json[astronaut].hoursInSpace}</li>
+         <li>Active: ${json[astronaut].active}</li>
+         <li>Skills: ${json[astronaut].skills.join(", ")}</li>
+      </ul>
+   </div>
+   <img class="avatar" src="${json[astronaut].picture}">
+</div>`
+    }
+    //container.innerHTML += allAstronaut;
+
+    });
+});
+});
